8c47589e3d08cc8806eeae94e72e63702539b599
arshkaur9091 <arshkaur9091@gmail.com>
2024-12-05 11:56:08 +0530

    sudoku puzzle backtracking

---
+++ Sudoku.java
@@ -0,0 +1,112 @@
+import java.util.*;
+
+public class Sudoku {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the size of the chessboard (N): ");
+        int n = scn.nextInt();
+
+        Sudoku sudokuSolver = new Sudoku();
+        List<List<String>> solutions = sudokuSolver.solveNQueens(n);
+
+        if (solutions.isEmpty()) {
+            System.out.println("No solutions exist for N = " + n);
+        } else {
+            System.out.println("Solutions for " + n + "-Queens Problem:");
+            for (List<String> solution : solutions) {
+                for (String row : solution) {
+                    System.out.println(row);
+                }
+                System.out.println(); // Separate each solution
+            }
+        }
+    }
+
+    // Rest of the code remains the same
+    public boolean isSafe(int row, int col, char[][] board) {
+        // Horizontal
+        for (int j = 0; j < board.length; j++) {
+            if (board[row][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Vertical
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Upper left
+        int r = row;
+        for (int c = col; c >= 0 && r >= 0; c--, r--) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
+
+        // Upper right
+        r = row;
+        for (int c = col; c < board.length && r >= 0; r--, c++) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
+
+        // Lower left
+        r = row;
+        for (int c = col; c >= 0 && r < board.length; r++, c--) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
+
+        // Lower right
+        for (int c = col; c < board.length && r < board.length; c++, r++) {
+            if (board[r][c] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void saveBoard(char[][] board, List<List<String>> allBoards) {
+        List<String> newBoard = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            StringBuilder row = new StringBuilder();
+            for (int j = 0; j < board[0].length; j++) {
+                row.append(board[i][j]);
+            }
+            newBoard.add(row.toString());
+        }
+        allBoards.add(newBoard);
+    }
+
+    public void helper(char[][] board, List<List<String>> allBoards, int col) {
+        if (col == board.length) {
+            saveBoard(board, allBoards);
+            return;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (isSafe(row, col, board)) {
+                board[row][col] = 'Q';
+                helper(board, allBoards, col + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> allBoards = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+        helper(board, allBoards, 0);
+        return allBoards;
+    }
+}
